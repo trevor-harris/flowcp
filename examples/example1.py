2e27820497265c2b64e0380eed4b03044c7cc2fb
Trevor Harris <tharris@tamu.edu>
2026-02-18 15:41:33 -0500

    updated example 1 to show score error is 0

--- examples/example1.py
+++ examples/example1.py
@@ -3,7 +3,7 @@ import jax
 import jax.numpy as jnp
 jax.config.update("jax_enable_x64", True)
 
-#### generate fake data
+#### generate "data" and "predictions"
 key = jax.random.key(0)
 key, key_cal, key_hat, key_test = jax.random.split(key, 4)
 
@@ -18,14 +18,18 @@ l2_score = flowcp.score.l2_nonconf()
 alpha = 0.1
 l2_score.fit(ycal, ycal_hat, alpha)
 
-# sample at 20 steps, init from ycal
+# sample
 l2_samp = flowcp.flow(l2_score, ycal, ytest_hat, 20)
-print(l2_samp.shape)
+
+# verify score error
+print(l2_score.score(l2_samp, ytest_hat) - l2_score.tau)
 
 #### sample across a range of alphas
 alpha = jnp.linspace(0.0, 1.0, ycal.shape[0])
 l2_score.fit(ycal, ycal_hat, alpha)
 
-# sample at 20 steps, init from ycal
+# sample
 l2_samp = flowcp.flow(l2_score, ycal, ytest_hat, 20)
-print(l2_samp.shape)
+
+# verify score error
+print(l2_score.score(l2_samp, ytest_hat) - l2_score.tau)
